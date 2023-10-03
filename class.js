f2d6fe603dcdc61242f71f7bfb3fed6238e04b2e
zain-student <zainmalik4989@gmail.com>
2023-10-03 12:48:32 +0500

    Class

---
+++ class.js
@@ -0,0 +1,11 @@
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+
+const myCar1 = new Car("Ford", 2014);
+const myCar2 = new Car("Audi", 2019);
+
+console.log("Car 1=" + myCar1, "Car 2=" + myCar2);
